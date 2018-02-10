ba813ddfe8808ad674e9150d97e17ea20df4486d
Stas D <iqpirat@gmail.com>
2018-02-10 16:54:42 +0200

    build: advanced js support

--- src/angular.build.ts
+++ src/angular.build.ts
@@ -1,1 +1,15 @@
+import { OperatorType, OrderBy, Query } from "./objects/query";
+
 export * from './objects/query'
+
+declare global {
+    interface Window {
+        Query: any;
+        OperatorType: any;
+        OrderBy: any;
+    }
+}
+
+window.Query = Query || {};
+window.OperatorType = OperatorType || {};
+window.OrderBy = OrderBy || {};
